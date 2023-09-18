0302b4d50594ea1e134e33df43de9bae8eab317c
Annabelle M <wouabelle@gmail.com>
2023-09-18 10:00:49 +0200

    ajout de boutons retour sur les comptes

--- src/app/components/newcompteepargne/newcompteepargne.component.ts
+++ src/app/components/newcompteepargne/newcompteepargne.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { BanckAccountDto } from 'src/app/dtos/BanckAccountDto';
 import { CompteBancaire } from 'src/app/models/compte-bancaire-model';
@@ -57,4 +57,11 @@ export class NewcompteepargneComponent implements OnInit {
       }
     );
   }
+  onBackButton() {
+    // Recupération de l'id en param URL
+this.route.paramMap.subscribe((params: ParamMap) => {
+  const id = params.get('id');
+  this.router.navigateByUrl(`/comptesbancaires/${id}`);
+});
+}
 }
